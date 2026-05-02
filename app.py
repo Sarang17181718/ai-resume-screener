c066207813c1d4a70829e67791a2af3fbfec5c86
Sarang <sarangbhokse29@example.com>
2026-05-03 00:23:50 +0530

    test deployment

--- app.py
+++ app.py
@@ -724,10 +724,7 @@ def logout():
     return redirect("/login")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True )
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True )
 
-
-#if __name__ == "__main__":
- #   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
